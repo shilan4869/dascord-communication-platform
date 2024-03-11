425040b1746a02803f56b4e5ce98c33d51d7b24f
shilan4869 <shilan4869@gmail.com>
2024-03-11 11:25:46 +0700

    #1 - Create Dascord App Using NextJS and mySQL

---
+++ src/app/(auth)/(routes)/login/[[...login]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignIn } from "@clerk/nextjs"
+
+const LogInPage = () => {
+  return <SignIn />
+}
+
+export default LogInPage
---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { UserButton } from "@clerk/nextjs"
+
+const Home = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </div>
+  )
+}
+
+export default Home
